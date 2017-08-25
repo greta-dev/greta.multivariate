b51c1a90ef5df4a97127506e10202966d05b3dc2
Nick Golding <goldingn35@gmail.com>
2017-08-25 15:07:49 +1000

    fill out package info

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' @title Multivariate Modelling with greta
+#' @name gretaMultivariate
+#'
+#' @description A greta extension providing distributions and functions for
+#' multivariate modelling (modelling multivariate response variables).
+#'
+#' @docType package
+#' @importFrom greta .internals
+#'
+NULL
